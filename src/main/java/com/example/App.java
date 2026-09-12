64865f0df5e232ed6e7fc75bffbcc064322d317b
Sravan Kumar <madthashravankumar2000@gmail.com>
2026-09-12 19:26:20 +0530

    Initial Commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("     DevOps Demo Application");
+        System.out.println("================================");
+        System.out.println("Application: Online");
+        System.out.println("Environment: Development");
+        System.out.println("Version: 1.0");
+    }
+}
